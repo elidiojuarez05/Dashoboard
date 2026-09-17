b8bba21d809078b0875fdf26e841a23fd1a42853
Code By Juarez <elidiojuarez05@gmail.com>
2026-09-16 19:07:17 -0600

    Add files via upload

--- run_app.py
+++ run_app.py
@@ -1,22 +1,22 @@
-"""Lanzador opcional para ejecutar el dashboard Streamlit."""
-import os
-import sys
 import streamlit.web.cli as stcli
-
+import os, sys
 
 def resolve_path(path):
-    if getattr(sys, "frozen", False):
+    if getattr(sys, 'frozen', False):
+        # BUSCA DENTRO DEL EXE
         return os.path.join(sys._MEIPASS, path)
     return os.path.abspath(path)
 
-
 if __name__ == "__main__":
-    dashboard = resolve_path(os.path.join("dashboard", "dashboard.py"))
+    # Importante: headless=true evita que intente abrir 
+    # una ventana de navegador antes de tiempo
     sys.argv = [
-        "streamlit",
-        "run",
-        dashboard,
-        "--server.port=8501",
-        "--server.headless=true",
+        "streamlit", "run", resolve_path("dashboard.py"),
+        "--server.port=8501", "--server.headless=true"
     ]
     stcli.main()
+
+#bordes del login
+div[data-testid="stForm"] {
+            border: none !important;
+            padding: 0 !important}
